a7d8c55efcbcc8be581943f2d900b9c4375cb0a4
planetenamek <35628060+planetenamek@users.noreply.github.com>
2018-03-07 15:13:40 +0100

    Fixed duplicated code

--- app.js
+++ app.js
@@ -20,55 +20,6 @@ bot.on("message", async message => {
  if(message.author.bot) return
 
  if(message.content.indexOf(config.prefix) !== 0) return
-
- if(command === "help") {
-  return cmd.help(message);
- }
-
- if(command === "last-post") {
-  return cmd.getLastPost(message);
- }
-
- if(command === "search") {
-  return cmd.getSearchContent(message);
- }
-
- if(command === "bal") {
-  return cmd.getWallet(message);
- }    
-});
-
-bot.login(config.token);
-
-
-
-
-
-
-
-
-const Discord = require("discord.js");
-const steem = require("steem");
-const bot = new Discord.Client();
-
-var config = require("./config.json");
-
-var streamOp = require("./actions/streamOp.js");
-var cmd = require("./cmd-bot.js");
-
-bot.on("ready", () => {
- console.log("App-Stream-V2 Ready !"); 
- bot.user.setActivity("Stream Steem !");
- streamOp.stream();
-}); 
-
-bot.on("message", async message => {
- const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
- const command = args.shift().toLowerCase()
-
- if(message.author.bot) return
-
- if(message.content.indexOf(config.prefix) !== 0) return
  
  if(command === "created") {
   return cmd.getCreatedContent(message);
@@ -93,6 +44,15 @@ bot.on("message", async message => {
 });
 
 bot.login(config.token);
+
+
+
+
+
+
+
+
+
 
 
 
