00c8f347ce761e43082db8139bb585c87276f928
planetenamek <35628060+planetenamek@users.noreply.github.com>
2018-03-08 13:15:24 +0100

    Fixed bug data.image

--- actions/discussionBFD.js
+++ actions/discussionBFD.js
@@ -6,19 +6,27 @@ const config = require("./../config.json");
 exports.discussionBFD = function(value,author,limit,message) {
  if(value === "single-content"){
   steem.api.getDiscussionsByAuthorBeforeDate(author,null, new Date().toISOString().split('.')[0],limit , function(err, result) {
-   if(result !== undefined && result.length > 0 ){
+   if(typeof result != "undefined"){
     let rep = steem.formatter.reputation(result[0].author_reputation),
         data = JSON.parse(result[0].json_metadata),
         tag = data.tags,
-        img = data.image,
+        img = "",
         embed = new Discord.RichEmbed();
+    
+    if(typeof data.image != "undefined"){
+      img = data.image;
+    }
 
     embed.setColor("#7DDF64")
          .setTitle("**" + result[0].title + "**") 
          .setDescription("**pending payout : " + result[0].pending_payout_value + " - Votes : " + result[0].net_votes + "**")
          .setAuthor( result[0].author + " (" + rep + ")")
-         .setThumbnail(img[0])
          .setURL("https://busy.org" + result[0].url)
+    
+    if(typeof img != "undefined"){
+      .setThumbnail(img[0])
+    }
+    
     return message.channel.send({embed});
    }else{
     return message.channel.send("Sorry user not found !");
