347b60bb4ea5c22e4faaf6069f5ac2539e0d88a6
planetenamek <ameplanetenamek@gmail.com>
2018-04-09 23:59:18 +0200

    Migration from  on steem-module.js

--- actions/steem-module.js
+++ actions/steem-module.js
@@ -12,6 +12,29 @@ var redFish = 999999,
 bot.login(config.token)
 
 module.exports = {
+// Get the created steem feed
+created : function(tag,limit,message) {
+ if(message.member.permissions.has("ADMINISTRATOR") || message.member.permissions.has("MANAGE_CHANNELS") ){
+  if(limit > 0 && limit <= 100) {    
+   steem.api.getDiscussionsByCreated({"tag": tag[0], "limit": limit}, function(err, result) {
+    if(result){
+     for (i = 1; i < result.length; i++) {
+      let author = result[i]["author"],
+          permlink = result[i]["permlink"]
+      message.channel.send("https://busy.org/" + tag + "/@" + author + "/" + permlink);
+     }                
+    }else{
+      return message.channel.send("Command invalid ! Please try again or type $help");
+    }  
+   });
+  }else{
+   return message.channel.send("Sorry the limit from this command is min 1 post and max 100 post ! Try again");
+  }
+}else{
+ return message.channel.send("You are not allowed to perform this action. (Admin or Mod only)");
+}
+},
+
 // Get the content with author (steem) and permlink
 getContent : function(author,permlink,idChannel) {
  steem.api.getContent(author,permlink, function (err,result) {
