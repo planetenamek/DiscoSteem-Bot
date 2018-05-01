f1a676ef8a345ab657e7f2a1ceaea33b036d821d
NGUYEN DINH Quoc-Huy <contact@quoc-huy.com>
2018-05-01 19:08:22 +1000

    Fix some uncatched exception error

--- actions/streamOp.js
+++ actions/streamOp.js
@@ -21,19 +21,24 @@ function stream() {
 
       // Check all posts published on the steem blockchain
       if (operation[0] == "comment" && operation[1].parent_author == "") {
-        let data = JSON.parse(operation[1].json_metadata),
-          tag = data.tags;
-        if (typeof tag != "undefined") {
-          if (tag.indexOf(config.principalTag) != (-1)) {
-            let author = operation[1].author,
-              permlink = operation[1].permlink;
-            for (v in config.altTags) {
-              if (tag.indexOf(v) != (-1)) {
-                content.getContent(author, permlink, config.altTags[v]);
+        try {
+          let data = JSON.parse(operation[1].json_metadata),
+            tag = data.tags;
+          if (typeof tag != "undefined") {
+            if (tag.indexOf(config.principalTag) != (-1)) {
+              let author = operation[1].author,
+                permlink = operation[1].permlink;
+              for (v in config.altTags) {
+                if (tag.indexOf(v) != (-1)) {
+                  content.getContent(author, permlink, config.altTags[v]);
+                }
               }
+              content.getContent(author, permlink, config.principalChan);
             }
-            content.getContent(author, permlink, config.principalChan);
           }
+        } catch (err) {
+          console.log("Error : " + err);
+          return;
         }
       } else if (operation[0] == "vote") {  // Check all votes from steem blockchain
         let voter = operation[1].voter,
@@ -57,7 +62,10 @@ function stream() {
               .setTimestamp();
 
             if (typeof tag != "undefined" && tag.indexOf(config.tagTrackerVoter) != (-1)) {
-              bot.channels.get(config.maintenance).send({embed});
+              let channel = bot.channels.get(config.maintenance);
+              if(channel) {
+                channel.send({embed});
+              }
             }
           });
         }
